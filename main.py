66809b1a54aaa7738c3f9c5250b38016055c78f6
meteor314 <meteor3141592@gmail.com>
2024-10-20 20:27:25 +0200

    update failed images

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from auth import authenticate
 from download import get_photos, download_image
 from database import create_database, insert_photo, is_photo_downloaded
-
+import sqlite3
 
 def main():
     creds = authenticate()
@@ -33,6 +33,37 @@ def main():
             print(f"Failed to download photo {photo_id}.")
             insert_photo(photo_id, filename, 'Failed')  # status of download
 
+
+    # download all failed images 
+    print("Downloading failed images")
+
+    failed_images = get_failed_images()
+    for failed_image in failed_images:
+        photo_id = failed_image[0]
+        filename = failed_image[1]
+
+        base_url = f"https://photoslibrary.googleapis.com/v1/mediaItems/{photo_id}"
+
+        if download_image(base_url, filename):
+            print(f"Photo {photo_id} downloaded successfully.")
+            insert_photo(photo_id, filename, 'Downloaded')
+        else:
+            print(f"Failed to download photo {photo_id}.")
+            insert_photo(photo_id, filename, 'Failed')
+
+
+def get_failed_images():
+
+
+    conn = sqlite3.connect('photos.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM photos WHERE status = 'Failed'")
+    failed_images = cursor.fetchall()
+
+    conn.close()
+
+    return failed_images
 
 if __name__ == '__main__':
     main()
